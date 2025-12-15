9ca6393cedda692998d388c9ad32f688e08c34a6
Xoka74 <ya.vladimir565@mail.ru>
2025-12-15 15:17:37 +0500

    Add logo to Login Screen

--- app/src/main/java/com/shurdev/t_prep/presentation/screens/login/LoginScreen.kt
+++ app/src/main/java/com/shurdev/t_prep/presentation/screens/login/LoginScreen.kt
@@ -1,10 +1,12 @@
 package com.shurdev.t_prep.presentation.screens.login
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,11 +15,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImage
+import com.shurdev.t_prep.R
 import com.shurdev.t_prep.data.managers.AccountManager
 import com.shurdev.t_prep.presentation.components.ErrorView
 import com.shurdev.t_prep.presentation.screens.login.viewModel.LoginIdleState
@@ -27,6 +32,7 @@ import com.shurdev.t_prep.presentation.screens.login.viewModel.LoginViewModel
 import com.shurdev.t_prep.presentation.screens.login.composables.SignInWithGoogleButton
 import com.shurdev.t_prep.presentation.components.layout.Center
 import com.shurdev.t_prep.ui.theme.TPrepTheme
+import com.shurdev.t_prep.utils.runSuspendCatching
 import kotlinx.coroutines.launch
 
 
@@ -53,8 +59,10 @@ fun LoginScreen(
 
     fun onLoginClick() {
         coroutineScope.launch {
-            val response = accountManager.login()
-            viewModel.login(response)
+            runSuspendCatching {
+                val response = accountManager.login()
+                viewModel.login(response)
+            }
         }
     }
 
@@ -72,6 +80,12 @@ fun LoginContent(
     error: String? = null,
 ) {
     Center {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.app_icon),
+            contentDescription = stringResource(R.string.start_screen),
+            modifier = Modifier.size(200.dp, 150.dp)
+        )
+
         Spacer(Modifier.height(10.dp))
 
         SignInWithGoogleButton(
