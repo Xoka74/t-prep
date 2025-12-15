05c17790ec863a95809d946286ed0fc921de09de
Xoka74 <ya.vladimir565@mail.ru>
2025-12-15 15:17:53 +0500

    Add user loading from backend

--- app/src/main/java/com/shurdev/t_prep/data/api/MeApi.kt
+++ app/src/main/java/com/shurdev/t_prep/data/api/MeApi.kt
@@ -1,7 +1,9 @@
 package com.shurdev.t_prep.data.api
 
 import com.shurdev.t_prep.data.models.MeUserDto
+import retrofit2.http.GET
 
 interface MeApi {
+    @GET("users/me")
     suspend fun getMeUser() : MeUserDto
 }
--- app/src/main/java/com/shurdev/t_prep/data/repositories/MeRepositoryImpl.kt
+++ app/src/main/java/com/shurdev/t_prep/data/repositories/MeRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.shurdev.t_prep.data.repositories
 
 import com.shurdev.t_prep.data.api.MeApi
+import com.shurdev.t_prep.data.mappers.toDomainModel
 import com.shurdev.t_prep.domain.models.MeUser
 import javax.inject.Inject
 
@@ -8,10 +9,6 @@ class MeRepositoryImpl @Inject constructor(
     private val meApi: MeApi,
 ) : MeRepository {
     override suspend fun getUser(): MeUser {
-        return MeUser(
-            1,
-            "TestUser"
-        )
-//        return meApi.getMeUser().toDomainModel()
+        return meApi.getMeUser().toDomainModel()
     }
 }
